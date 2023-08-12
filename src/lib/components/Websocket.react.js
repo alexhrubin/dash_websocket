4b87ad2c7bb9f951154fb4b036db8df59e4139ee
Alex Rubin <ahrubin@ucdavis.edu>
2023-08-12 12:43:57 -0700

    SSE

--- src/lib/components/Websocket.react.js
+++ src/lib/components/Websocket.react.js
@@ -6,31 +6,47 @@ export default class CustomHTTPComponent extends Component {
     super(props);
   }
 
-  // Function to send a message and await a response
+  // Function to send a message and await a response with SSE
   sendMessage = async (message) => {
-    console.log(message)
     try {
       const response = await fetch(this.props.url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(message),
       });
-      const data = await response.json();
-      console.log(data)
-      if (data) {
-        this.props.setProps({ message: data }); // Update parent with received message
+
+      const reader = response.body.getReader();
+      let accumulator = ""; // Accumulator to store the chunks
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        
+        // Convert the Uint8Array to a string and add to the accumulator
+        accumulator += new TextDecoder().decode(value);
+  
+        // Split the accumulator into lines and process each line
+        let lines = accumulator.split("\r\n\r\n");
+        for (let i = 0; i < lines.length - 1; i++) {
+          const line = lines[i];
+          const data = JSON.parse(line);
+          this.props.setProps({ message: data });
+        }
+  
+        // Leave the last (potentially incomplete) line in the accumulator
+        accumulator = lines[lines.length - 1];
       }
     } catch (error) {
       console.error('Error sending message:', error);
     }
-    // Clear the "send" property so the same message can be sent again
-    this.props.setProps({ send: null });
-  };
+  
+};
 
-  componentDidUpdate(prevProps) {
+componentDidUpdate(prevProps) {
     // If the send prop is set, send the message
     if (this.props.send) {
-      this.sendMessage(this.props.send);
+        this.sendMessage(this.props.send);
+        this.props.setProps({ send: null });
     }
   }
 
